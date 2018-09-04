4e2a9e3ecd280f303ddfe1cbbb93bb51ce20b4f3
vurdurio <42960808+vurdurio@users.noreply.github.com>
2018-09-04 15:15:56 +0700

    Create vurduio.py

---
+++ vurduio.py
@@ -0,0 +1,301 @@
+class vurdurio:
+    def vurdur1(self, ins):
+        if ins: return True
+        else: return False
+    def vurdur2(self, ins):
+        a = self.vurdur1(ins)
+        return a
+    def vurdur3(self, ins):
+        a = self.vurdur2(ins)
+        return a
+    def vurdur4(self, ins):
+        a = self.vurdur3(ins)
+        return a
+    def vurdur5(self, ins):
+        a = self.vurdur4(ins)
+        return a
+    def vurdur6(self, ins):
+        a = self.vurdur5(ins)
+        return a
+    def vurdur7(self, ins):
+        a = self.vurdur6(ins)
+        return a
+    def vurdur8(self, ins):
+        a = self.vurdur7(ins)
+        return a
+    def vurdur9(self, ins):
+        a = self.vurdur8(ins)
+        return a
+    def vurdur10(self, ins):
+        a = self.vurdur9(ins)
+        return a
+    def vurdur11(self, ins):
+        a = self.vurdur10(ins)
+        return a
+    def vurdur12(self, ins):
+        a = self.vurdur11(ins)
+        return a
+    def vurdur13(self, ins):
+        a = self.vurdur12(ins)
+        return a
+    def vurdur14(self, ins):
+        a = self.vurdur13(ins)
+        return a
+    def vurdur15(self, ins):
+        a = self.vurdur14(ins)
+        return a
+    def vurdur16(self, ins):
+        a = self.vurdur15(ins)
+        return a
+    def vurdur17(self, ins):
+        a = self.vurdur16(ins)
+        return a
+    def vurdur18(self, ins):
+        a = self.vurdur17(ins)
+        return a
+    def vurdur19(self, ins):
+        a = self.vurdur18(ins)
+        return a
+    def vurdur20(self, ins):
+        a = self.vurdur19(ins)
+        return a
+    def vurdur21(self, ins):
+        a = self.vurdur20(ins)
+        return a
+    def vurdur22(self, ins):
+        a = self.vurdur21(ins)
+        return a
+    def vurdur23(self, ins):
+        a = self.vurdur22(ins)
+        return a
+    def vurdur24(self, ins):
+        a = self.vurdur23(ins)
+        return a
+    def vurdur25(self, ins):
+        a = self.vurdur24(ins)
+        return a
+    def vurdur26(self, ins):
+        a = self.vurdur25(ins)
+        return a
+    def vurdur27(self, ins):
+        a = self.vurdur26(ins)
+        return a
+    def vurdur28(self, ins):
+        a = self.vurdur27(ins)
+        return a
+    def vurdur29(self, ins):
+        a = self.vurdur28(ins)
+        return a
+    def vurdur30(self, ins):
+        a = self.vurdur29(ins)
+        return a
+    def vurdur31(self, ins):
+        a = self.vurdur30(ins)
+        return a
+    def vurdur32(self, ins):
+        a = self.vurdur31(ins)
+        return a
+    def vurdur33(self, ins):
+        a = self.vurdur32(ins)
+        return a
+    def vurdur34(self, ins):
+        a = self.vurdur33(ins)
+        return a
+    def vurdur35(self, ins):
+        a = self.vurdur34(ins)
+        return a
+    def vurdur36(self, ins):
+        a = self.vurdur35(ins)
+        return a
+    def vurdur37(self, ins):
+        a = self.vurdur36(ins)
+        return a
+    def vurdur38(self, ins):
+        a = self.vurdur37(ins)
+        return a
+    def vurdur39(self, ins):
+        a = self.vurdur38(ins)
+        return a
+    def vurdur40(self, ins):
+        a = self.vurdur39(ins)
+        return a
+    def vurdur41(self, ins):
+        a = self.vurdur40(ins)
+        return a
+    def vurdur42(self, ins):
+        a = self.vurdur41(ins)
+        return a
+    def vurdur43(self, ins):
+        a = self.vurdur42(ins)
+        return a
+    def vurdur44(self, ins):
+        a = self.vurdur43(ins)
+        return a
+    def vurdur45(self, ins):
+        a = self.vurdur44(ins)
+        return a
+    def vurdur46(self, ins):
+        a = self.vurdur45(ins)
+        return a
+    def vurdur47(self, ins):
+        a = self.vurdur46(ins)
+        return a
+    def vurdur48(self, ins):
+        a = self.vurdur47(ins)
+        return a
+    def vurdur49(self, ins):
+        a = self.vurdur48(ins)
+        return a
+    def vurdur50(self, ins):
+        a = self.vurdur49(ins)
+        return a
+    def vurdur51(self, ins):
+        a = self.vurdur50(ins)
+        return a
+    def vurdur52(self, ins):
+        a = self.vurdur51(ins)
+        return a
+    def vurdur53(self, ins):
+        a = self.vurdur52(ins)
+        return a
+    def vurdur54(self, ins):
+        a = self.vurdur53(ins)
+        return a
+    def vurdur55(self, ins):
+        a = self.vurdur54(ins)
+        return a
+    def vurdur56(self, ins):
+        a = self.vurdur55(ins)
+        return a
+    def vurdur57(self, ins):
+        a = self.vurdur56(ins)
+        return a
+    def vurdur58(self, ins):
+        a = self.vurdur57(ins)
+        return a
+    def vurdur59(self, ins):
+        a = self.vurdur58(ins)
+        return a
+    def vurdur60(self, ins):
+        a = self.vurdur59(ins)
+        return a
+    def vurdur61(self, ins):
+        a = self.vurdur60(ins)
+        return a
+    def vurdur62(self, ins):
+        a = self.vurdur61(ins)
+        return a
+    def vurdur63(self, ins):
+        a = self.vurdur62(ins)
+        return a
+    def vurdur64(self, ins):
+        a = self.vurdur63(ins)
+        return a
+    def vurdur65(self, ins):
+        a = self.vurdur64(ins)
+        return a
+    def vurdur66(self, ins):
+        a = self.vurdur65(ins)
+        return a
+    def vurdur67(self, ins):
+        a = self.vurdur66(ins)
+        return a
+    def vurdur68(self, ins):
+        a = self.vurdur67(ins)
+        return a
+    def vurdur69(self, ins):
+        a = self.vurdur68(ins)
+        return a
+    def vurdur70(self, ins):
+        a = self.vurdur69(ins)
+        return a
+    def vurdur71(self, ins):
+        a = self.vurdur70(ins)
+        return a
+    def vurdur72(self, ins):
+        a = self.vurdur71(ins)
+        return a
+    def vurdur73(self, ins):
+        a = self.vurdur72(ins)
+        return a
+    def vurdur74(self, ins):
+        a = self.vurdur73(ins)
+        return a
+    def vurdur75(self, ins):
+        a = self.vurdur74(ins)
+        return a
+    def vurdur76(self, ins):
+        a = self.vurdur75(ins)
+        return a
+    def vurdur77(self, ins):
+        a = self.vurdur76(ins)
+        return a
+    def vurdur78(self, ins):
+        a = self.vurdur77(ins)
+        return a
+    def vurdur79(self, ins):
+        a = self.vurdur78(ins)
+        return a
+    def vurdur80(self, ins):
+        a = self.vurdur79(ins)
+        return a
+    def vurdur81(self, ins):
+        a = self.vurdur80(ins)
+        return a
+    def vurdur82(self, ins):
+        a = self.vurdur81(ins)
+        return a
+    def vurdur83(self, ins):
+        a = self.vurdur82(ins)
+        return a
+    def vurdur84(self, ins):
+        a = self.vurdur83(ins)
+        return a
+    def vurdur85(self, ins):
+        a = self.vurdur84(ins)
+        return a
+    def vurdur86(self, ins):
+        a = self.vurdur85(ins)
+        return a
+    def vurdur87(self, ins):
+        a = self.vurdur86(ins)
+        return a
+    def vurdur88(self, ins):
+        a = self.vurdur87(ins)
+        return a
+    def vurdur89(self, ins):
+        a = self.vurdur88(ins)
+        return a
+    def vurdur90(self, ins):
+        a = self.vurdur89(ins)
+        return a
+    def vurdur91(self, ins):
+        a = self.vurdur90(ins)
+        return a
+    def vurdur92(self, ins):
+        a = self.vurdur91(ins)
+        return a
+    def vurdur93(self, ins):
+        a = self.vurdur92(ins)
+        return a
+    def vurdur94(self, ins):
+        a = self.vurdur93(ins)
+        return a
+    def vurdur95(self, ins):
+        a = self.vurdur94(ins)
+        return a
+    def vurdur96(self, ins):
+        a = self.vurdur95(ins)
+        return a
+    def vurdur97(self, ins):
+        a = self.vurdur96(ins)
+        return a
+    def vurdur98(self, ins):
+        a = self.vurdur97(ins)
+        return a
+    def vurdur99(self, ins):
+        a = self.vurdur98(ins)
+        return a
+    def vurduriomu(self, ins):
+        a = self.vurdur99(ins)
+        return a
